06a064d7f440671e49889bfd6cb69fc79f64534e
haxul <s.s.starodubov@yandex.ru>
2020-04-16 20:00:05 +0400

    init apai

--- api/src/test/java/com/posws/PoswsApplicationTests.java
+++ api/src/test/java/com/posws/PoswsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.haxul.hh;
+package com.posws;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HhApplicationTests {
+class PoswsApplicationTests {
 
     @Test
     void contextLoads() {
